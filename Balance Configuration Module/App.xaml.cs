0628ab8e68153d6d9fddd0ea39659d68d013359f
Agurall <shenya_59_59@mail.ru>
2020-02-19 12:43:56 +0300

    Init #2

---
+++ Balance Configuration Module/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Balance_Configuration_Module
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
